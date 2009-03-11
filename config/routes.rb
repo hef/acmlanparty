338cbeb960640704a2f578fbd9d1941a2a797400
Hef <hef+storm@pbrfrat.com>
2009-03-10 20:38:50 -0500

    although it doesn do anything, activation works

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ ActionController::Routing::Routes.draw do |map|
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
+  map.resources :users, :member => { :suspend   => :put,
+      :unsuspend => :put,
+      :purge     => :delete }
 
   map.resource :session
 
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
